cdaf0adc6a09e2ae615931d32e844c7ffa9fe2c4
Niloy Saha <niloysaha.ns@gmail.com>
2023-10-14 15:03:46 -0400

    Update subscribers

--- mongo-tools/add-subscriber.py
+++ mongo-tools/add-subscriber.py
@@ -6,12 +6,14 @@ from data import sub_data
 MONGO_URI = "localhost"
 MONGO_PORT = 27017
 
+
 def add_subscriber():
-    subscribers = ["subscriber_1", "subscriber_2", "subscriber_3"]
+    subscribers = ["subscriber_1", "subscriber_2", "subscriber_3", "subscriber_4"]
     for subscriber in subscribers:
         Open5GS_1 = Open5GS(MONGO_URI, MONGO_PORT)
         sub_data[subscriber]["_id"] = ObjectId()
         Open5GS_1.add_subscriber(sub_data[subscriber])
+
 
 if __name__ == "__main__":
     run_with_port_forwarding(add_subscriber)
--- mongo-tools/data.py
+++ mongo-tools/data.py
@@ -12,18 +12,18 @@ slice_data = {
                 "pcc_rule": [],
                 "ambr": {
                     "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
-                    "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps}
+                    "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
                 },
                 "qos": {
                     "index": 9,
                     "arp": {
                         "priority_level": 8,
                         "pre_emption_capability": Open5GS.Status.DISABLED,
-                        "pre_emption_vulnerability": Open5GS.Status.DISABLED
-                    }
-                }
+                        "pre_emption_vulnerability": Open5GS.Status.DISABLED,
+                    },
+                },
             }
-        ]
+        ],
     },
     "slice_2": {
         "sst": 2,
@@ -31,30 +31,30 @@ slice_data = {
         "default_indicator": True,
         "session": [
             {
-                "name": "internet",
+                "name": "streaming",
                 "type": Open5GS.Type.IPv4,
                 "pcc_rule": [],
                 "ambr": {
                     "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
-                    "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps}
+                    "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
                 },
                 "qos": {
                     "index": 7,
                     "arp": {
                         "priority_level": 8,
                         "pre_emption_capability": Open5GS.Status.DISABLED,
-                        "pre_emption_vulnerability": Open5GS.Status.DISABLED
-                    }
-                }
+                        "pre_emption_vulnerability": Open5GS.Status.DISABLED,
+                    },
+                },
             }
-        ]
-    }
+        ],
+    },
 }
 
 
 sub_data = {
     "subscriber_1": {
-        "_id": '',
+        "_id": "",
         "imsi": "001010000000001",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
@@ -63,19 +63,19 @@ sub_data = {
         "slice": [slice_data["slice_1"]],
         "ambr": {
             "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
-            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps}
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
             "k": "465B5CE8B199B49FAA5F0A2EE238A6BC",
             "amf": "8000",
             "op": None,
-            "opc": "E8ED289DEBA952E4283B54E88E6183CA"
+            "opc": "E8ED289DEBA952E4283B54E88E6183CA",
         },
         "schema_version": 1,
-        "__v": 0
+        "__v": 0,
     },
     "subscriber_2": {
-        "_id": '',
+        "_id": "",
         "imsi": "001010000000002",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
@@ -84,19 +84,19 @@ sub_data = {
         "slice": [slice_data["slice_2"]],
         "ambr": {
             "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
-            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps}
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
             "k": "B199B49F465B5CE8E238A6BCAA5F0A2E",
             "amf": "8000",
             "op": None,
-            "opc": "283B54E8E8ED289D8E6183CAEBA952E4"
+            "opc": "283B54E8E8ED289D8E6183CAEBA952E4",
         },
         "schema_version": 1,
-        "__v": 0
+        "__v": 0,
     },
     "subscriber_3": {
-        "_id": '',
+        "_id": "",
         "imsi": "001010000060592",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
@@ -105,15 +105,36 @@ sub_data = {
         "slice": [slice_data["slice_1"]],
         "ambr": {
             "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
-            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps}
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
             "k": "1B9DC14B6E16A8FE83AA0E8A0AB56FCB",
             "amf": "8000",
             "op": None,
-            "opc": "8CD505786285C50FEC35AD9D328816EA"
+            "opc": "8CD505786285C50FEC35AD9D328816EA",
         },
         "schema_version": 1,
-        "__v": 0
-    }
+        "__v": 0,
+    },
+    "subscriber_4": {
+        "_id": "",
+        "imsi": "001010000060594",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "6526B07605B6D5663FD947C5BA65D141",
+            "amf": "8000",
+            "op": None,
+            "opc": "FA25065FAC14E0EB5196BAD382B72C09",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
 }
