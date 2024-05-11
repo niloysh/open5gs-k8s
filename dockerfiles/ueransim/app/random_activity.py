05fa431edcd77471faea6eff39befd5dc5463efc
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-05-11 01:24:17 +0000

    bind address properly for random act

--- dockerfiles/ueransim/app/random_activity.py
+++ dockerfiles/ueransim/app/random_activity.py
@@ -1,8 +1,15 @@
 import os
 import random
 import time
+import subprocess
+
+def getAddress():
+    cmd = "ip a show uesimtun0 | grep inet | head -n 1 | awk '{print $2}'"
+    result = subprocess.check_output(cmd, shell=True)
+    return result
 
 while True:
+    address = getAddress
     act = random.randint(0, 5)
     if act == 0:
         os.system("ping -I uesimtun0 google.com > /tmp/$LOGFILE 2>&1 & -c 100")
@@ -12,8 +19,11 @@ while True:
         os.system("curl --interface uesimtun0 ")
     elif act == 3:
         url = "https://www.youtube.com/watch?v=rHvhsQ9dvJI"
-        os.system(f"yt-dlp -P ~/Videos {url}")
+        os.system(f"yt-dlp --source-address {address} -P ~/Videos {url}")
+        time.sleep(75)
         os.system("rm -dr ~/Videos")
-        
+    elif act == 4:
+        url = ""
+        os.system(f"wget --bind-address {address} {url}")
     sleeptime = random.randint(2, 7)
     time.sleep(sleeptime)
