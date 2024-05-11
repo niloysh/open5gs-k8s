deea64032755957dc99c3994bff25c5be471d774
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-05-11 00:29:58 +0000

    Add youtube scenario

--- dockerfiles/ueransim/app/random_activity.py
+++ dockerfiles/ueransim/app/random_activity.py
@@ -10,6 +10,10 @@ while True:
         os.system("ping -I uesimtun0 facebook.com > /tmp/$LOGFILE 2>&1 & -c 100")
     elif act == 2:
         os.system("curl --interface uesimtun0 ")
+    elif act == 3:
+        url = "https://www.youtube.com/watch?v=rHvhsQ9dvJI"
+        os.system(f"yt-dlp -P ~/Videos {url}")
+        os.system("rm -dr ~/Videos")
         
     sleeptime = random.randint(2, 7)
     time.sleep(sleeptime)
