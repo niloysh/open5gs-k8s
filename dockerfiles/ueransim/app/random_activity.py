12437b6861b38b7fd106a7a75b44e9bd5e651324
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-05-11 01:30:08 +0000

    Update CURL and delete the file from wget

--- dockerfiles/ueransim/app/random_activity.py
+++ dockerfiles/ueransim/app/random_activity.py
@@ -16,14 +16,17 @@ while True:
     elif act == 1:
         os.system("ping -I uesimtun0 facebook.com > /tmp/$LOGFILE 2>&1 & -c 100")
     elif act == 2:
-        os.system("curl --interface uesimtun0 ")
+        url = "https://en.wikipedia.org/wiki/5G"
+        os.system(f"curl --interface uesimtun0 {url}")
     elif act == 3:
         url = "https://www.youtube.com/watch?v=rHvhsQ9dvJI"
         os.system(f"yt-dlp --source-address {address} -P ~/Videos {url}")
         time.sleep(75)
         os.system("rm -dr ~/Videos")
     elif act == 4:
-        url = ""
+        url = "https://releases.ubuntu.com/focal/ubuntu-20.04.6-desktop-amd64.iso"
+        file = url.split('/')[-1]
         os.system(f"wget --bind-address {address} {url}")
+        os.system(f"rm {file}")
     sleeptime = random.randint(2, 7)
     time.sleep(sleeptime)
