4f86f212d40b012947a3e0d6c05653ffb38e875d
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-05-11 03:20:06 +0000

    Update node and add some print statements

--- dockerfiles/ueransim/app/random_activity.py
+++ dockerfiles/ueransim/app/random_activity.py
@@ -17,21 +17,26 @@ def run_action(act):
     logfile = f"/tmp/{current_time}_logfile.txt"
 
     if act == 0:
+        print('Ping Google')
         os.system(f"ping -I uesimtun0 google.com -c 100 2>&1 | tee {logfile}")
     elif act == 1:
+        print('Ping FB')
         os.system(f"ping -I uesimtun0 facebook.com -c 100 2>&1 | tee {logfile}")
     elif act == 2:
+        print('Curl Wiki')
         url = random.randint(0, 1)
         if url == 0: url = "https://en.wikipedia.org/wiki/5G"
         else: url = "https://en.wikipedia.org/wiki/Jitter"
         os.system(f"curl --interface uesimtun0 {url} 2>&1 | tee {logfile}")
     elif act == 3:
+        print('Youtube')
         url = "https://www.youtube.com/watch?v=rHvhsQ9dvJI"
         os.makedirs("./Videos", exist_ok=True)
         os.system(f"./yt-dlp --source-address {address} -P ./Videos {url} 2>&1 | tee {logfile}")
         time.sleep(75)
         os.system("rm -dr ./Videos")
     elif act == 4:
+        print('Download Ubunutu ISO')
         url = "https://releases.ubuntu.com/focal/ubuntu-20.04.6-desktop-amd64.iso"
         file = url.split('/')[-1]
         os.system(f"wget --bind-address {address} {url} 2>&1 | tee {logfile}")
